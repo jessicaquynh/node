b587c1f2a7f21fc00b5c116d48a9c44d62feae11
jessicaquynh <jessica.quynh.tran@gmail.com>
2016-10-25 20:21:32 -0400

    implementing recommended changes from matteo
    This returns the length of the buffer's head in with byteLength
    function. It also updates a test to test for allocation of a
    mixed, split buffer.

--- test/parallel/test-stream-readable-null-buffer-head.js
+++ test/parallel/test-stream-readable-null-buffer-head.js
@@ -3,30 +3,45 @@ const common = require('../common');
 const Readable = require('_stream_readable');
 const assert = require('assert');
 
-var buf = '';
 const emdash = new Buffer([0xE2, 0x80, 0x94]);
 const euro = new Buffer([0xE2, 0x82, 0xAC]);
-const source = Buffer.concat([emdash, euro]);
+const source = Buffer.concat([emdash, euro, Buffer.from('asdfasl')]);
+const errorMsg = 'howMuchToRead attempts to retrieve data from an empty buffer.head';
 
-const readable = Readable({ encoding: 'utf8' });
+exactBuffer();
+oddNumberBuffer();
 
-readable.push(source.slice(0, 4));
-readable.push(source.slice(4, 6));
-readable.push(null);
+var exactBuf = '';
+function exactBuffer() {
+  var readable = Readable({ encoding: 'utf8' });
+  readable.push(source);
+  readable.push(null);
 
-readable.on('data', data => {
-  console.log(data);
-  process.nextTick = () => {
-    if (readable._readableState.length !== 0 &&
-        readable._readableState.flowing &&
-        readable._readableState.buffer.head == null) {
-      throw new Error(`howMuchToRead attempts to retrieve data from an empty buffer.head`);
-    }
-  };
+  readable.on('data', data => {
+    exactBuf += data;
+  });
+}
 
-  buf += data;
+var oddBuf = '';
+function oddNumberBuffer() {
+  var readable = Readable({ encoding: 'utf8' });
+
+  readable.push(source.slice(0, 2));
+  readable.push(source.slice(2, 4));
+  readable.push(source.slice(4, 14));
+  readable.push(null);
+
+  readable.on('data', data => {
+    oddBuf += data;
+  });
+}
+
+process.on('uncaughtException', (e) => {
+  console.log(e);
+  throw new Error(errorMsg);
 });
 
 process.on('exit', function() {
-  assert.strictEqual(buf, '—€');
+  assert.strictEqual(exactBuf, '—€asdfasl');
+  assert.strictEqual(oddBuf, '—€asdfasl');
 });
